0781e3673d87038e5a1edbc1d8ff7be68b79dd27
David vanMaanen <david@vanmaanen.us>
2015-04-20 11:33:12 -0400

    Updated routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,38 +1,86 @@
 'use strict';
 
 var personalSite = angular.module('personalSite',[
-    'ngRoute',
+    'ui.router',
     'siteControllers',
     'siteServices'
 
 ]);
-
+personalSite.run([
+    '$rootScope', '$state', '$stateParams',
+    function($rootScope, $state, $stateParams){
+	$rootScope.$state = $state;
+	$rootScope.$stateParams = $stateParams;
+    }
+]);
+    
+    
 personalSite.config([
-    '$routeProvider',
-    function($routeProvider) {
-	$routeProvider.
-	    when('/home',{
+    '$stateProvider','$urlRouterProvider',
+    function($stateProvider,$urlRouterProvider) {
+	$urlRouterProvider.
+	    when('/resume','/resume/about').
+	    otherwise('/');
+	$stateProvider.
+	    state('home',{
+		url:'/',
 		templateUrl: 'views/home.html',
-		controller: 'HomeCtrl'
+		controller: 'HomeCtrl',
+		data:{
+		    title:'Home',
+		},
 	    }).
-	    when('/blog',{
-		templateUrl: 'views/blogList.html',
+	    state('blog',{
+		abstract: true,
+		url:'/blog/',
+		templateUrl: 'views/blog.html',
+		controller: 'BlogListCtrl',
+		data:{
+		    title:'Blog',
+		},
+	    }).
+	    state('blog.list',{
+		url:'',
+		templateUrl: 'views/blog-list.html',
 		controller: 'BlogListCtrl'
 	    }).
-	    when('/blog/:blogId',{
-		templateUrl: 'views/blog.html',
+	    state('blog.detail',{
+		url:'/:blogId',
+		templateUrl: 'views/blog-detail.html',
 		controller: 'BlogCtrl'
 	    }).
-	    when('/resume',{
+	    state('resume',{
+		url:'/resume',
 		templateUrl: 'views/resume.html',
-		controller: 'ResumeCtrl'
+		controller: 'ResumeCtrl',
+		data:{
+		    title:'Resume'
+		},
 	    }).
-	    when('/projects',{
+	    state('resume.exp',{
+		url:'/exp',
+		templateUrl: 'views/resume-exp.html',
+	    }).
+	    state('resume.skills',{
+		url:'/skills',
+		templateUrl: 'views/resume-skills.html',
+
+	    }).
+	    state('resume.education',{
+		url:'/edu',
+		templateUrl: 'views/resume-edu.html',
+	    }).
+	    state('resume.about',{
+		url:'/about',
+		templateUrl: 'views/resume-about.html',
+	    }).	    
+	    state('projects',{
+		url:'/projects',
 		templateUrl: 'views/projects.html',
-		controller: 'ProjectsCtrl'
-	    }).
-	    otherwise({
-		redirectTo: '/home'
+		controller: 'ProjectsCtrl',
+		data:{
+		    title:'Projects'
+		}
 	    })
     }]);
     
