058e467dcbeb88894ee5f27eac175ed296c80df2
David vanMaanen <david@vanmaanen.us>
2015-06-08 11:38:36 -0400

    Reformatted states to use params, rather than one big file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,111 +22,16 @@ personalSite.config([
 	    when('/resume','/resume/about').
 	    otherwise('/');
 	$stateProvider.
-	    state('home',{
-		url:'/',
-		templateUrl: 'views/home.html',
-		controller: 'HomeCtrl',
-		data:{
-		    title:'Home',
-		},
-	    }).
-	    state('blog',{
-		abstract: true,
-		url:'/blog/',
-		templateUrl: 'views/blog.html',
-		controller: 'BlogListCtrl',
-		data:{
-		    title:'Blog',
-		},
-	    }).
-	    state('blog.list',{
-		url:'',
-		templateUrl: 'views/blog-list.html',
-		controller: 'BlogListCtrl'
-	    }).
-	    state('blog.detail',{
-		url:'/:blogId',
-		templateUrl: 'views/blog-detail.html',
-		controller: 'BlogCtrl'
-	    }).
-	    state('resume',{
-		url:'/resume',
-		templateUrl: 'views/resume.html',
-
-		data:{
-		    title:'Resume'
-		},
-	    }).
-	    state('resume.exp',{
-		url:'/exp',
-		resolve:{
-		    ResolveData: function(Resume){
-			var exp=Resume.exp_cats.get('', function(){
-			    for (var i=0; i< exp.results.length; i++){
-				var tp=exp.results[i];
-				for (var j=0; j<tp.jobs.length; j++){
-				    tp.jobs[j].start=moment(tp.jobs[j].start).format('MMMM YYYY');
-				    tp.jobs[j].end=new moment(tp.jobs[j].end).format('MMMM YYYY');
-				    console.log(tp.jobs[j].start);
-				}
-			    }
-
-
-			});
-			return exp
-
-		    }
-		},
-		templateUrl: 'views/resume-exp.html',
-		controller: function($scope,ResolveData){
-		    console.log(ResolveData);
-		    $scope.data=ResolveData;
-		},
-	    }).
-	    state('resume.skills',{
-		url:'/skills',
-		resolve:{
-		    ResolveData: function(Resume){
-			return Resume.skill_levels.get()
-		    }
-		},
-		templateUrl: 'views/resume-skills.html',
-		controller: 'ResumeCtrl',
-	    }).
-	    state('resume.education',{
-		url:'/edu',
-		resolve:{
-		    ResolveData: function(Resume){
-			return {edu: Resume.education.get(),
-			       courses: Resume.courses.get()}
-		    }
-		},
-		templateUrl: 'views/resume-edu.html',
-		controller: 'ResumeCtrl',
-	    }).
-	    state('resume.about',{
-		url:'/about',
-		resolve:{
-		    ResolveData: function(Resume){
-			return {about:"About"}
-		    }
-		},
-		templateUrl: 'views/resume-about.html',
-		controller: 'ResumeCtrl',
-	    }).	    
-	    state('projects',{
-		url:'/projects',
-		resolve:{
-		    projects: function(Projects){
-			return Projects.projects.get().$promise;			       
-		    }	    
-		},
-		controller: 'ProjectsCtrl',
-		data:{
-		    title:'Projects'
-		},
-		templateUrl: 'views/projects.html',
-	    });
+	    state('home',HomeStateParams).
+	    state('blog',BlogStateParams).
+	    state('blog.list',BlogListStateParams).
+	    state('blog.detail',BlogDetailStateParams).
+	    state('resume',ResumeStateParams).
+	    state('resume.exp',ResumeExpStateParams).
+	    state('resume.skills',ResumeSkillsStateParams).
+	    state('resume.education',ResumeEduStateParams).
+	    state('resume.about',ResumeAboutStateParams).	    
+	    state('projects',ProjectsStateParams);
     }]);
     
 				   
